709c40b35ebf1910d8ea34811f5ec1df12f14d3c
petezhang <895061650@qq.com>
2017-06-10 22:44:52 +0800

    banner

--- js/slider.js
+++ js/slider.js
@@ -2,5 +2,13 @@
  * Created by Administrator on 2017/6/10.
  */
 $(document).ready(function () {
-console.log('111')
+    // 初始化
+    init();
+
+    function init() {
+        $('#img').onload=function () {
+            alert('111')
+
+        }
+    }
 })
